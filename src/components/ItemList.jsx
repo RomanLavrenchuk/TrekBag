d38a83e4919dc0acab356ea2cf94973e3f0360c3
Roman <polskaago@gmail.com>
2024-11-07 21:39:33 -0500

    Sorting Items

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react';
 import EmptyView from './EmptyView';
+import Select from 'react-select';
 
+const sortingOptions = [
+    {
+        label: 'Sort by default',
+        value: 'default',
+    },
+    {
+        label: 'Sort by packed',
+        value: 'packed',
+    },
+    {
+        label: 'Sort by unpacked',
+        value: 'unpacked',
+    },
+];
 export default function ItemList({
     items,
     handleDeleteItem,
     handleToggleItem,
 }) {
+    const [sortBy, setSortBy] = useState('default');
+    const sortedItems = [...items].sort((a, b) => {
+        if (sortBy === 'packed') {
+            return b.packed - a.packed;
+        }
+        if (sortBy === 'unpacked') {
+            return a.packed - b.packed;
+        }
+    });
     return (
-        <ul className=''>
-            {items.length === 0 && <EmptyView />}
-            {items.map((item) => {
+        <ul className='item-list'>
+            {items.length === 0 ? <EmptyView /> : null}
+
+            {items.length > 0 ? (
+                <section className='sorting'>
+                    <Select
+                        onChange={(option) => setSortBy(option.value)}
+                        defaultValue={sortingOptions[0]}
+                        options={sortingOptions}
+                    />
+                </section>
+            ) : null}
+            {sortedItems.map((item) => {
                 return (
                     <Item
                         key={item.id}
